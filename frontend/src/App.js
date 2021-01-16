a81c143daeeecabbc21838cbf6394d1943ce0eb8
Francisco Junior <machado.fjunior@gmail.com>
2021-01-16 11:55:19 -0300

    Alterado App para function no lugar de classe e melhorado a função responsável por apresentar os componentes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,49 @@
-import React from 'react'
-import RegisterForm from './components/RegisterForm/RegisterForm'
-import ListClients from './components/ListClients/ListClients'
-import { Container, Typography, Breadcrumbs, Button } from '@material-ui/core'
-import 'fontsource-roboto'
+import React, { useState } from "react";
+import RegisterForm from "./components/RegisterForm/RegisterForm";
+import ListClients from "./components/ListClients/ListClients";
+import { ValidCPF } from "./models/validation";
+import { Container, Typography, Breadcrumbs, Button } from "@material-ui/core";
+import "fontsource-roboto";
 
-function handleClick(e) {
-  e.preventDefault()
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [component] = useState([
+    <RegisterForm submitForm={submitForm} validations={{ cpf: ValidCPF }} />,
+    <ListClients />,
+  ]);
 
-  let section = document.getElementById(e.target.textContent)
-  let button = e.target
-
-  if (section.style.display === 'none' && section.id === 'listar') {
-    section.style.display = ''
-    button.textContent = 'cadastrar'
-    document.getElementById(e.target.textContent).style.display = 'none'
-  } else {
-    section.style.display = ''
-    button.textContent = 'listar'
-    document.getElementById(e.target.textContent).style.display = 'none'
+  function handleClick(e) {
+    e.preventDefault();
+    if (counter === 0) {
+      setCounter(1);
+      e.target.textContent = "cadastrar";
+    } else {
+      setCounter(0);
+      e.target.textContent = "listar";
+    }
   }
-}
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Container component="header" maxWidth="lg" >
-          <Typography component="h1" variant="h2" align="center" >C.R.U.D</Typography>
-          <Breadcrumbs style={{ margin: "15px auto", maxWidth: "max-content" }}>
-            <Button variant="contained" color="primary" onClick={handleClick}>
-              listar
-            </Button>
-          </Breadcrumbs>
-        </Container>
-        <Container component="article" maxWidth="md">
-          <RegisterForm submitForm={submitForm} />
-          <ListClients />
-        </Container>
-      </>
-    )
-  }
+  return (
+    <>
+      <Container component="header" maxWidth="lg">
+        <Typography component="h1" variant="h2" align="center">
+          C.R.U.D
+        </Typography>
+        <Breadcrumbs style={{ margin: "15px auto", maxWidth: "max-content" }}>
+          <Button variant="contained" color="primary" onClick={handleClick}>
+            listar
+          </Button>
+        </Breadcrumbs>
+      </Container>
+      <Container component="article" maxWidth="md">
+        {component[counter]}
+      </Container>
+    </>
+  );
 }
 
 function submitForm(dados) {
-  console.log(dados)
+  console.log(dados);
 }
 
-export default App
+export default App;
